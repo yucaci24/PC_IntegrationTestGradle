645ba3de1fdc2c88922517f4a1c0976b6ff12db5
Yuliana Cañas Cifuentes <yuliana.canas@ceiba.com.co>
2018-11-21 10:45:15 -0500

    Organizando proyecto

--- src/test/java/org/gradle/PersonTestFixture.java
+++ src/test/java/org/gradle/PersonTestFixture.java
@@ -1,10 +1,18 @@
 package org.gradle;
 
+import org.junit.Test;
 import static org.junit.Assert.*;
+
 
 public class PersonTestFixture {
     public static Person create(String name) {
         assertNotNull(name);
         return new Person(name);
+    }
+
+    @Test
+    public void canConstructAPersonWithAName() {
+        Person person = new Person("Larry");
+        assertEquals("Larry", person.getName());
     }
 }
